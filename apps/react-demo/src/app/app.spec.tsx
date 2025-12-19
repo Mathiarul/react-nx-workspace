c9414c580d921df0ad5e597362fc2a534318bc29
Mathiarul Gunasekaran <mathiarul.g@sapiens.com>
2025-12-19 04:18:02 +0100

    added a ui lib

--- apps/react-demo/src/app/app.spec.tsx
+++ apps/react-demo/src/app/app.spec.tsx
@@ -13,15 +13,4 @@ describe('App', () => {
     expect(baseElement).toBeTruthy();
   });
 
-  it('should have a greeting as the title', () => {
-    const { getAllByText } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-    expect(
-      getAllByText(new RegExp('Welcome @react-demo/react-demo', 'gi')).length >
-        0
-    ).toBeTruthy();
-  });
 });
